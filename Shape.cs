89b9d07d9555a0499f7c77a1ed95017b3d33db16
Joakim Eberholst <J_eberholst@hotmail.com>
2021-12-22 20:03:57 +0100

    Add project files.

---
+++ Shape.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Shape
+{
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+    public abstract override string ToString();
+    public void SayHello()
+    {
+        Console.WriteLine($"{this.GetType().Name} says hello!");
+    }
+    public virtual void ScreamHello()
+    {
+        Console.WriteLine($"{this.GetType().Name.ToUpper()} SCREAMS HELLO!");
+    }
+    //Notera att SayHello inte kommer att kunna ändras i de ärvda klasserna.
+    //ScreamHello kan dock ändras då det är markerat som virtual.
+
+
+}
+
+public class Square: Shape
+{
+
+    public Square(int side)
+    {
+        SideLength = side;
+    }
+
+    public int SideLength { get; set; }
+
+
+    public override double GetArea()
+    {
+        return (SideLength * SideLength);
+    }
+
+    public override double GetPerimeter()
+    {
+        return (SideLength * 4);
+    }
+
+    public override string ToString()
+    {
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("This is a ");
+        sb.Append(this.GetType().Name.ToLower());
+        sb.Append(" ");
+        sb.Append("with side length " + SideLength);
+        sb.Append(", ");
+        sb.Append("area " + GetArea().ToString());
+        sb.Append(" ");
+        sb.Append("and perimeter length " + GetPerimeter().ToString());
+        sb.Append(".");
+
+        return sb.ToString();
+    }
+}
+
+public class Rectangle: Shape
+{
+
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public Rectangle(int width)
+    {
+        Width = width;
+        Height = width;
+    }
+
+    public override double GetArea()
+    {
+        return (Width * Height);
+    }
+
+    public override double GetPerimeter()
+    {
+        return (Width * 2) + (Height * 2);
+    }
+
+    public override string ToString()
+    {
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("This is a ");
+        sb.Append(this.GetType().Name.ToLower());
+        sb.Append(" ");
+        sb.Append("with height " + Height);
+        sb.Append(" ");
+        sb.Append("and width " + Width);
+        sb.Append(". ");
+        sb.Append("Area is " + GetArea().ToString());
+        sb.Append(" ");
+        sb.Append("and perimeter is " + GetPerimeter().ToString());
+        sb.Append(".");
+
+
+        return sb.ToString();
+
+    }
+
+}
+
+
+
+
+public class Circle : Shape
+{
+    private int radius;
+
+    public Circle(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return ((radius * radius) * Math.PI);
+    }
+
+    public override double GetPerimeter()
+    {
+        return ((radius * 2) * Math.PI);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("This is a ");
+        sb.Append(this.GetType().Name.ToLower());
+        sb.Append(" ");
+        sb.Append("with radius " + radius);
+        sb.Append(".");
+        sb.Append(" ");
+        sb.Append("The area is " + GetArea());
+        sb.Append(" ");
+        sb.Append("and the perimeter is " + GetPerimeter().ToString());
+        sb.Append(".");
+
+
+        return sb.ToString();
+    }
+}
+
